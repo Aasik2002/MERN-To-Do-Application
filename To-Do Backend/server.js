a23ab59c3dee8c978bcd8d22b30a3c6e37ca1daa
Aasik2002 <aasikfa2002@gmail.com>
2026-03-04 12:55:56 +0530

    API testing is Finsh with Postman

--- To-Do Backend/server.js
+++ To-Do Backend/server.js
@@ -4,10 +4,33 @@ const express = require('express');
 // Create an instance of Express
 const app = express();
 
+app.use(express.json());
 // Define a route for the root URL
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+// app.get('/', (req, res) => {
+//   res.send('Hello, World!');
+// });
+
+// sample in-memory data store for todo items
+let todos = [];
+
+// create a new todo item
+app.post('/todos',(req,res)=>{
+  const {title,discription} = req.body;
+  const newtodo = {
+    id :todos.length + 1,
+    title,
+    discription
+  }
+  todos.push(newtodo);
+  console.log(todos);
+  res.status(201).json(newtodo);
+})
+
+// Get all todo items
+app.get('/todos',(req,res)=>{
+  res.json(todos);
 });
+
 
 // Start the server on port 3000
 const PORT = 3000;
